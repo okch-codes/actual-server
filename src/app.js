7db7709f0ce7e09904e6faafd7de49ce96c8635a
Omar Karim Chtioui <okchtioui@gmail.com>
2024-11-02 19:46:54 +0000

    trust any proxy

--- src/app.js
+++ src/app.js
@@ -14,6 +14,8 @@ import * as secretApp from './app-secrets.js';
 
 const app = express();
 
+app.set('trust proxy', true);
+
 process.on('unhandledRejection', (reason) => {
   console.log('Rejection:', reason);
 });
